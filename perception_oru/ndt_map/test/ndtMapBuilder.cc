9ec3628c2c301d28ad386da1370b4ce6aaf78735
jean <j.z.feng@foxmail.com>
2018-04-23 17:22:36 +0800

    ndt_map

--- perception_oru/ndt_map/test/ndtMapBuilder.cc
+++ perception_oru/ndt_map/test/ndtMapBuilder.cc
@@ -18,31 +18,53 @@
 #include <boost/thread/mutex.hpp>
 
 #include <Eigen/Eigen>
-
-static int ctr = 0, ctr2 = 0;
+#include <string>
+int ctr = 0, ctr2 = 0;
 static boost::mutex mutex;
 static bool fresh = false;
 
 void ndtCallback(const sensor_msgs::PointCloud2ConstPtr &msg) {
     pcl::PointCloud<pcl::PointXYZ> cloud;
     pcl::fromROSMsg(*msg, cloud);
-
     ROS_INFO ("Received %d data points with the following fields: %s", (int)(msg->width * msg->height),
               pcl::getFieldsList (*msg).c_str ());
 
-    lslgeneric::NDTMap nd(new lslgeneric::LazyGrid(2));
+    lslgeneric::NDTMap nd(new lslgeneric::LazyGrid(0.4));
 
     Eigen::Affine3d T;
     T.setIdentity();
     nd.addPointCloud(T.translation(), cloud);
     nd.computeNDTCells();
 
-    ROS_INFO("Loaded point cloud");
-    if (ctr % 10 == 0) {
-        nd.writeToJFF("maps02.jff");
-    }
-    ctr++;
+    ROS_INFO("Loaded point cloud, %d's time", ctr);
+	
+	nd.writeToJFF("1.jff");// const char*
 
+    ctr++;
+}
+
+void convert(){
+	pcl::PointCloud<pcl::PointXYZ> cloud;
+	ROS_INFO ("start converting");
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>("1422133476050525.pcd", cloud) == -1) //* load the file
+    {
+    // we can change name to ohter, and every time we could just run the node
+    // TODO: change resolution, map size etc
+        ROS_INFO ("error of load points");
+        return;
+    }
+	ROS_INFO ("loading ok!");
+	lslgeneric::NDTMap nd(new lslgeneric::LazyGrid(0.4));
+
+    Eigen::Affine3d T;
+    T.setIdentity();
+
+	nd.initialize(T.translation()(0), T.translation()(1), T.translation()(2), 300, 300, 5);
+	nd.addPointCloud(T.translation(), cloud, 0.1, 100.0, 0.1);
+
+    nd.computeNDTCells(CELL_UPDATE_MODE_SAMPLE_VARIANCE, 1e5, 255, T.translation(), 0.1);
+	
+	nd.writeToJFF("2.jff");// const char*
 }
 
 int
@@ -50,8 +72,9 @@ main (int argc, char** argv) {
 
     ros::init(argc, argv, "ndt_builder");
     ros::NodeHandle n;
-    ros::Subscriber sub1 = n.subscribe("points2_in", 10, ndtCallback);
-    ros::spin();
+    //ros::Subscriber sub1 = n.subscribe("/velodyne_cloud_registered_all", 100, ndtCallback);
+    //ros::spin();
+	convert();
 
     return (0);
 }
--- perception_oru/ndt_map/test/ndtMapBuilder.cpp
+++ perception_oru/ndt_map/test/ndtMapBuilder.cpp
@@ -24,34 +24,54 @@ static boost::mutex mutex;
 static bool fresh = false;
 
 void ndtCallback(const sensor_msgs::PointCloud2ConstPtr &msg) {
+	std::string name = "map";
     pcl::PointCloud<pcl::PointXYZ> cloud;
     pcl::fromROSMsg(*msg, cloud);
-
+	name+=std::to_string(ctr);
     ROS_INFO ("Received %d data points with the following fields: %s", (int)(msg->width * msg->height),
               pcl::getFieldsList (*msg).c_str ());
 
-    lslgeneric::NDTMap nd(new lslgeneric::LazyGrid(2));
+    lslgeneric::NDTMap nd(new lslgeneric::LazyGrid(0.4));
 
     Eigen::Affine3d T;
     T.setIdentity();
     nd.addPointCloud(T.translation(), cloud);
     nd.computeNDTCells();
 
-    ROS_INFO("Loaded point cloud");
-    if (ctr % 10 == 0) {
-        nd.writeToJFF("maps02.jff");
-    }
-    ctr++;
+    ROS_INFO("Loaded point cloud, %d's time", ctr+1);
+	
+	nd.writeToJFF(name);// const char*
 
+    ctr++;
+}
+
+void convert(){
+	pcl::PointCloud<pcl::PointXYZ> cloud;
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>("1422133392235211.pcd", cloud) == -1) //* load the file
+    {
+        PCL_ERROR("Couldn't read file test_pcd.pcd \n");
+        return (-1);
+    }
+	lslgeneric::NDTMap nd(new lslgeneric::LazyGrid(0.4));
+
+    Eigen::Affine3d T;
+    T.setIdentity();
+    nd.addPointCloud(T.translation(), cloud);
+    nd.computeNDTCells();
+
+    ROS_INFO("Loaded point cloud, %d's time", ctr+1);
+	
+	nd.writeToJFF(name);// const char*
 }
 
 int
 main (int argc, char** argv) {
 
     ros::init(argc, argv, "ndt_builder");
-    ros::NodeHandle n;
-    ros::Subscriber sub1 = n.subscribe("points2_in", 10, ndtCallback);
-    ros::spin();
+    //ros::NodeHandle n;
+    //ros::Subscriber sub1 = n.subscribe("/velodyne_cloud_registered_all", 100, ndtCallback);
+    //ros::spin();
+	convert();
 
     return (0);
 }
