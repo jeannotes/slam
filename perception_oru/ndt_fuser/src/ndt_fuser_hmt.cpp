a077d35e14d85aa6a76cbaff5e257be911a1e0e5
jean <j.z.feng@foxmail.com>
2018-05-04 17:11:28 +0800

    ndt_fuser

--- perception_oru/ndt_fuser/src/ndt_fuser_hmt.cpp
+++ perception_oru/ndt_fuser/src/ndt_fuser_hmt.cpp
@@ -102,8 +102,8 @@ Eigen::Affine3d NDTFuserHMT::update(Eigen::Affine3d Tmotion, pcl::PointCloud<pcl
                 viewer->plotNDTSAccordingToOccupancy(-1, map);
                 //viewer->plotLocalNDTMap(cloud,resolution);
             }
-            viewer->addTrajectoryPoint(Tnow.translation()(0), Tnow.translation()(1), Tnow.translation()(2) + 0.2, 0, 1, 0);
-            viewer->addTrajectoryPoint(Todom.translation()(0), Todom.translation()(1), Todom.translation()(2) + 0.2, 0.5, 0, 0.5);
+            viewer->addTrajectoryPoint(Tnow.translation()(0), Tnow.translation()(1), Tnow.translation()(2) + 0.2, 1, 0, 0);
+            viewer->addTrajectoryPoint(Todom.translation()(0), Todom.translation()(1), Todom.translation()(2) + 0.2, 0, 1, 0);
             viewer->displayTrajectory();
             viewer->setCameraPointing(Tnow.translation()(0), Tnow.translation()(1), Tnow.translation()(2) + 3);
             viewer->repaint();
