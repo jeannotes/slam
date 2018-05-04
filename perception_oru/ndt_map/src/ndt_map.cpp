5029e87aa86196b7bec4a8c21e1b62aa1083fa64
jean <j.z.feng@foxmail.com>
2018-05-04 17:11:42 +0800

    ndt_map

--- perception_oru/ndt_map/src/ndt_map.cpp
+++ perception_oru/ndt_map/src/ndt_map.cpp
@@ -26,10 +26,10 @@ namespace lslgeneric {
 */
 void NDTMap::loadPointCloud(const pcl::PointCloud<pcl::PointXYZ> &pc, double range_limit) {
     if (index_ != NULL) {
-        std::cout << "CLONE INDEX\n";
+        //std::cout << "CLONE INDEX\n";
         SpatialIndex *si = index_->clone();
         if (!isFirstLoad_) {
-            std::cout << "deleting old index\n";
+            //std::cout << "deleting old index\n";
             delete index_;
         }
         isFirstLoad_ = false;
@@ -123,9 +123,6 @@ void NDTMap::loadPointCloud(const pcl::PointCloud<pcl::PointXYZ> &pc, double ran
         }
     }
 
-    fprintf(stderr, "centroid is");
-    fprintf(stderr, "maxDist is");
-
     it = pc.points.begin();
     while (it != pc.points.end()) {
         Eigen::Vector3d d;
@@ -154,7 +151,6 @@ void NDTMap::loadPointCloud(const pcl::PointCloud<pcl::PointXYZ> &pc, double ran
 
     isFirstLoad_ = false;
 }
-
 
 /**
 * loadPointCloudCentroid - A special load function to enable the matching of centroids (create alligned maps)
